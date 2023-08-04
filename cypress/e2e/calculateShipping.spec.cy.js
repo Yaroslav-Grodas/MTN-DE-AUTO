e7f9bd03f38e4c59b7547412ac9295b2a9867bc7
Yaroslav Donchuk <donchukoff@gmail.com>
2023-08-04 14:36:53 +0300

    Adding shipping calculation spec

---
+++ cypress/e2e/calculateShipping.spec.cy.js
@@ -0,0 +1,200 @@
+/// <reference types="cypress" />
+
+describe('calculate shipping', () => {
+
+  beforeEach(() => {
+
+    cy.visit('https://shopmtn.de/');
+
+    cy.wait(5000);
+
+    cy.contains('.needsclick', 'CONTINUE TO MTN SHOP DE')
+      .click();
+
+    cy.wait(20000);
+
+    cy.get('[aria-label="Close form 3"]')
+      .click();
+  });  
+
+  it('should allow user to calculate shipping of the product FIRST case', () => {
+
+    cy.get('a[href="/pages/unsere-marken"]')
+      .click();
+
+    cy.wait(5000);
+
+    cy.contains('.gr-brands-list__item', 'Beneca')
+      .click();
+
+    cy.wait(10000);
+
+    cy.contains('.gr-card-rich-product__heading', 'Beneca 50 mm einteiliger Spanngurt')
+      .click();
+
+    cy.wait(2000);
+
+    cy.contains('.gr-summary__heading', 'Versandkostenrechner')
+      .click();
+
+    cy.get('#gr_shipping_calculator_country')
+      .select('Deutschland');
+
+    cy.get('#gr_shipping_calculator_zip')
+      .type('10115');
+
+    cy.contains('.gr-shipping-calc__submit', 'Versandkosten berechnen')
+      .click();
+
+    cy.get('.gr-shipping-calc__response')
+      .should('exist');
+     
+  });
+
+  it('should allow user to calculate shipping of the product SECOND case', () => {
+
+    cy.get('a[href="/pages/unsere-marken"]')
+      .click();
+
+    cy.wait(5000);
+
+    cy.contains('.gr-brands-list__item', 'Dunlop')
+      .click();
+
+    cy.wait(10000);
+
+    cy.contains('.gr-card-rich-product__heading', 'Dunlop Purofort Thermo+ Full Safety')
+      .click();
+
+    cy.wait(2000);
+
+    cy.contains('.gr-summary__heading', 'Versandkostenrechner')
+      .click();
+
+    cy.get('#gr_shipping_calculator_country')
+      .select('Belgien');
+
+    cy.get('#gr_shipping_calculator_zip')
+      .type('3831');
+
+    cy.contains('.gr-shipping-calc__submit', 'Versandkosten berechnen')
+      .click();
+
+    cy.get('.gr-shipping-calc__response')
+      .should('exist');
+
+  });
+
+  it('should allow user to calculate shipping of the product THIRD case', () => {
+
+    cy.get('a[href="/pages/unsere-marken"]')
+      .click();
+
+    cy.wait(5000);
+
+    cy.contains('.gr-brands-list__item', 'Hansen')
+      .click();
+
+    cy.wait(10000);
+
+    cy.contains('.gr-card-rich-product__heading', 'Hansen Protection Ersatz-Schrittgurt für Leisure Life Jacken 82964')
+      .click();
+
+    cy.wait(2000);
+
+    cy.contains('.gr-summary__heading', 'Versandkostenrechner')
+      .click();
+
+    cy.get('#gr_shipping_calculator_country')
+      .select('Niederlande');
+
+    cy.get('#gr_shipping_calculator_zip')
+      .type('3067 TT');
+
+    cy.contains('.gr-shipping-calc__submit', 'Versandkosten berechnen')
+      .click();
+
+    cy.get('.gr-shipping-calc__response')
+      .should('exist');
+
+  });
+
+  it('should allow user to calculate shipping of the product FOURTH case', () => {
+
+    cy.get('a[href="/pages/unsere-marken"]')
+      .click();
+
+    cy.wait(5000);
+
+    cy.contains('.gr-brands-list__item', 'Safetex')
+      .click();
+
+    cy.wait(10000);
+
+    cy.contains('.gr-card-rich-product__heading', 'Safetex Rundschlinge SX schwarz - 10 Stück')
+      .click();
+
+    cy.wait(2000);
+
+    cy.contains('.gr-summary__heading', 'Versandkostenrechner')
+      .click();
+
+    cy.get('#gr_shipping_calculator_country')
+      .select('Österreich');
+
+    cy.get('#gr_shipping_calculator_zip')
+      .type('4821');
+
+    cy.contains('.gr-shipping-calc__submit', 'Versandkosten berechnen')
+      .click();
+
+    cy.get('.gr-shipping-calc__response')
+      .should('exist');
+
+  });
+
+  it('negative scenarios in the shipping calculation', () => {
+
+    cy.get('a[href="/pages/unsere-marken"]')
+      .click();
+
+    cy.wait(5000);
+
+    cy.contains('.gr-brands-list__item', 'Crewsaver')
+      .click();
+
+    cy.wait(10000);
+
+    cy.contains('.gr-card-rich-product__heading', 'Crewsaver Premier Kinderschwimmweste 83205')
+      .click();
+
+    cy.wait(2000);
+
+    cy.contains('.gr-summary__heading', 'Versandkostenrechner')
+      .click();
+
+    cy.get('#gr_shipping_calculator_zip')
+      .type('kkk');
+
+    cy.contains('.gr-shipping-calc__submit', 'Versandkosten berechnen')
+      .click();
+
+    cy.get('.gr-shipping-calc__hint')
+      .should('contain.text', 'Bitte Land auswählen');
+
+    cy.wait(2000);
+
+    cy.get('#gr_shipping_calculator_country')
+      .select('Österreich');
+
+    cy.contains('.gr-shipping-calc__submit', 'Versandkosten berechnen')
+      .click();
+    
+    cy.contains('.gr-shipping-title', 'Error:')
+      .should('exist');
+
+    
+
+    
+  });
+});
