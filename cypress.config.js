f760511796e48326fdbbea8dde4d661085db40a7
Yaroslav Donchuk <donchukoff@gmail.com>
2023-11-21 19:47:33 +0200

    Adding retries to the tests

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   projectId: "bupmav",
   e2e: {
     baseUrl: 'https://shopmtn.de',
+    retries: {
+      runMode: 2,
+      openMode: 0
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
